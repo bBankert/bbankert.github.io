794d563aa338a4f93083d7dd0a883df6063a4d56
bBanker <fireninja171@gmail.com>
2019-12-17 08:33:48 -0500

    Added mobile responsiveness

--- scripts.js
+++ scripts.js
@@ -1,39 +1,10 @@
-
-
-function frame() {
-        if(pos >= 350){
-        y = Math.floor((Math.random()*300)+1);
-        elem.style.bottom = y +'px';
-        elem.style.left = 0;
-        pos = 0
-        document.getElementById("animate").innerHTML='y'+y;
-    }  else {
-        pos++;
-        elem.style.left = pos + 'px'; 
+function addActiveClass(id){
+    //remove active element
+    var activeElement = document.querySelector('.active');
+    if(activeElement){
+        activeElement.classList.remove('active');
     }
-
+    //set new active element
+    var clickedElement = document.getElementById(id);
+    clickedElement.classList.add('active');
 }
-function openNav(){
-    document.getElementById("navBar").style.width="250px";
-    document.getElementById("main").style.marginLeft="250px";
-}     
-
-function closeBar(){
-    document.getElementById("navBar").style.width="0";
-    document.getElementById("main").style.marginLeft="0";
-}
-    
-$(document).ready(function(){
-    $("a").on('click',function(event){
-        if(this.hash !== ""){
-            event.preventDefault();
-            
-            var hash = this.hash;
-            
-            $('html, body').animate({scrollTop: $(hash).offset().top},800,function(){
-                
-                window.location.hash = hash;
-            });
-        }
-    });
-});
